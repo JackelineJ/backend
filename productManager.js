bb03976ab48f5482b7b83093e68f438a4cdd4f5f
JackelineJ <jacky@rustningshield.com>
2023-03-27 19:18:53 -0300

    añadi verificacion de campos y return en getProductById

--- productManager.js
+++ productManager.js
@@ -5,6 +5,11 @@ class ProductManager {
 
     addProducts(title, description, price, thumbnail, code, stock) {
 
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log('Error: All fields must be provided for adding a new product');
+            return;
+        }
+
         const productAlredyExists = this.products.find(item => item.code === code);
         if (productAlredyExists) {
             console.log(`Error: A product with code ${code} already exists`);
@@ -27,21 +32,25 @@ class ProductManager {
         return this.products;
     }
 
-    getProductById (id) {
+    getProductById(id) {
         const product = this.products.find(element => element.id === id);
         if (!product) {
             console.log('Error: Product not found');
-        } else {
-            console.log(product)
+            return null;
         }
+        return product;
     }
 }
 
 const productManager = new ProductManager();
 console.log(productManager.getProducts()); // This return the empty array because there are no products yet
 
+productManager.addProducts('producto prueba', 'Este es un producto prueba', 200);
+console.log(productManager.getProducts()); 
+// This tries to add the new product but because it doesn't have all the fields complete yet it throws an error
+
 productManager.addProducts('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
-console.log(productManager.getProducts()); // This adds the new product
+console.log(productManager.getProducts()); // This adds the new product because it fills all the fields
 
 productManager.addProducts('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25);
 console.log(productManager.getProducts()); // When adding this, it will throw an error because it's the same product listed above
