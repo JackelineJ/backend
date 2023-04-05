ec20b66f0bea48713d7007eae95fac5099348821
JackelineJ <jacky@rustningshield.com>
2023-04-04 23:26:22 -0300

    uso de express server

--- serverExpress.js
+++ serverExpress.js
@@ -1,1 +1,24 @@
 const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('Hi this is my server')
+})
+
+app.get('/products', (request, response) => {
+    fs.readFile('productArchive.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            response.status(500).send('Error reading products file');
+            return;
+        }
+        const products = JSON.parse(data);
+        response.json(products);
+    });
+})
+
+app.listen(8080, ()=>{
+    console.log('Here is the 8080 port')
+})
